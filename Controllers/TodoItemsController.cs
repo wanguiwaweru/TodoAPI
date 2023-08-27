b01c35733cc10525099b41f44f72093e129e30d9
wanguiwaweru <wanguiwawerub@gmail.com>
2023-08-27 17:00:28 +0300

    Add client

--- Controllers/TodoItemsController.cs
+++ Controllers/TodoItemsController.cs
@@ -9,7 +9,7 @@ using TodoApi.Models;
 
 namespace TodoApi.Controllers
 {
-    [Route("api/[TodoItems]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class TodoItemsController : ControllerBase
     {
